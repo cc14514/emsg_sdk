a840cd827685c178f9eca583ffb55e8da4b5f8b7
13611348910 <1047235154@qq.com>
2014-07-09 16:38:43 +0800

    refactor test case

--- emsg-sdk-java/src/test/java/com/emsg/test/offlinemsg/customtype/OfflineMsgTester.java
+++ emsg-sdk-java/src/test/java/com/emsg/test/offlinemsg/customtype/OfflineMsgTester.java
@@ -1,7 +1,6 @@
-package com.emsg.test.offlinemsg.p2p;
+package com.emsg.test.offlinemsg.customtype;
 
 import junit.framework.Assert;
-
 import net.sf.json.JSONObject;
 
 import org.junit.After;
@@ -13,10 +12,14 @@ import com.emsg.client.PacketListener;
 import com.emsg.client.SimpleClient;
 
 /**
- * 测试离线消息存储数量（如10条），用户<-->用户场景：
+ * 
+ * 类型101离线消息存储数量（只存储最后条），用户<-->用户场景：
  * 用户liang（liang@test.com/123）给用户cai（cai@test.com/123）发消息，
- * 用户cai不在线，用户liang发了20条消息，用户cai上线后只能收到10条，
- * 且是最后收到的10条
+ * 用户cai不在线，用户liang发了10条消息，用户cai上线后只能收到最后一条。
+ * 
+ * 类型100离线消息存储数量（只存储第一条），用户<-->用户场景：
+ * 用户liang（liang@test.com/123）给用户cai（cai@test.com/123）发消息，
+ * 用户cai不在线，用户liang发了10条消息，用户cai上线后只能收到第一条。
  * 
  * @author tiger
  *
@@ -41,50 +44,14 @@ public class OfflineMsgTester {
     	sender.shutdown();
     	receiver.shutdown();
     }
-
+    
     @Test
-    public void testOfflineMsgCountLessThan() throws Exception {
+    public void testOfflineMsgType100() throws Exception {
     	// 在线用户给离线用户发送消息
     	sender.init(null);
     	Thread.sleep(1000);
-    	for (int i=0; i<9; i++) {
-    		sender.send(Constants.to_account, message+i);
-    	}
-
-    	// 等待消息保存为离线消息
-    	Thread.sleep(1000);
-    	
-    	// 离线用户上线，接收离线消息
-    	receiver.init(new PacketListener() {
-			@Override
-			public void processPacket(String packet) {
-				System.out.println("aaa recv ===> "+counter + "  "+packet);
-				
-				JSONObject jp = JSONObject.fromObject(packet);
-				JSONObject payload = jp.getJSONObject("payload");
-				String content = payload.getString("content");
-				Assert.assertTrue(content.equals(message + counter));
-				
-				// 使用计数器计算离线消息个数
-				counter++;
-			}
-		});
-    	
-    	// 等待离线消息被完全接收
-    	Thread.sleep(2000);
-    	
-    	// 判断离线消息数量
-    	Assert.assertTrue(counter == 9);
-    }
-
-
-    @Test
-    public void testOfflineMsgCount() throws Exception {
-    	// 在线用户给离线用户发送消息
-    	sender.init(null);
-    	Thread.sleep(1000);
-    	for (int i=0; i<20; i++) {
-    		sender.send(Constants.to_account, message+i);
+    	for (int i=0; i<10; i++) {
+    		sender.sendTypedMessage(Constants.to_account, 100, message+i);
     	}
 
     	// 等待消息保存为离线消息
@@ -99,7 +66,7 @@ public class OfflineMsgTester {
 				JSONObject jp = JSONObject.fromObject(packet);
 				JSONObject payload = jp.getJSONObject("payload");
 				String content = payload.getString("content");
-				Assert.assertTrue(content.equals(message + (counter + 10)));
+				Assert.assertTrue(content.equals(message + 0));
 				
 				// 使用计数器计算离线消息个数
 				counter++;
@@ -110,8 +77,42 @@ public class OfflineMsgTester {
     	Thread.sleep(2000);
     	
     	// 判断离线消息数量
-    	Assert.assertTrue(counter == Constants.OFFLINE_MSG_COUNT);
+    	Assert.assertTrue(counter == 1);
     }
+    
+    @Test
+    public void testOfflineMsgType101() throws Exception {
+    	// 在线用户给离线用户发送消息
+    	sender.init(null);
+    	Thread.sleep(1000);
+    	for (int i=0; i<10; i++) {
+    		sender.sendTypedMessage(Constants.to_account, 101, message+i);
+    	}
 
+    	// 等待消息保存为离线消息
+    	Thread.sleep(1000);
+    	
+    	// 离线用户上线，接收离线消息
+    	receiver.init(new PacketListener() {
+			@Override
+			public void processPacket(String packet) {
+				System.out.println("aaa recv ===> "+counter + "  "+packet);
 
+				JSONObject jp = JSONObject.fromObject(packet);
+				JSONObject payload = jp.getJSONObject("payload");
+				String content = payload.getString("content");
+				Assert.assertTrue(content.equals(message + 9));
+				
+				// 使用计数器计算离线消息个数
+				counter++;
+			}
+		});
+    	
+    	// 等待离线消息被完全接收
+    	Thread.sleep(2000);
+    	
+    	// 判断离线消息数量
+    	Assert.assertTrue(counter == 1);
+    }
+    
 }
--- emsg-sdk-java/src/test/java/com/emsg/test/offlinemsg/group/OfflineGroupMsgTester.java
+++ emsg-sdk-java/src/test/java/com/emsg/test/offlinemsg/group/OfflineGroupMsgTester.java
@@ -1,4 +1,4 @@
-package com.emsg.test.offlinemsg.p2p;
+package com.emsg.test.offlinemsg.group;
 
 import junit.framework.Assert;
 
@@ -13,15 +13,15 @@ import com.emsg.client.PacketListener;
 import com.emsg.client.SimpleClient;
 
 /**
- * 测试离线消息存储数量（如10条），用户<-->用户场景：
- * 用户liang（liang@test.com/123）给用户cai（cai@test.com/123）发消息，
- * 用户cai不在线，用户liang发了20条消息，用户cai上线后只能收到10条，
- * 且是最后收到的10条
+ * 测试离线消息存储数量（如10条），用户<-->群场景：
+ * 用户liang（1001）给群丰简（群号1）发消息，
+ * 群中用户cai（1002）不在线，用户liang发了20条消息，
+ * 用户cai上线后只能收到10条，用户liang自己不会收到群消息。
  * 
  * @author tiger
  *
  */
-public class OfflineMsgTester {
+public class OfflineGroupMsgTester {
 
 	// 消息计数器
 	public static int counter = 0;
@@ -32,8 +32,8 @@ public class OfflineMsgTester {
     @Before
     public void setUp() throws Exception {
     	counter = 0;
-    	sender = new SimpleClient(Constants.from_account, Constants.from_password);
-    	receiver = new SimpleClient(Constants.to_account, Constants.to_password);
+    	sender = new SimpleClient("1001@test.com/123", Constants.from_password);
+    	receiver = new SimpleClient("1002@test.com/123", Constants.to_password);
     }
 
     @After
@@ -41,56 +41,27 @@ public class OfflineMsgTester {
     	sender.shutdown();
     	receiver.shutdown();
     }
-
-    @Test
-    public void testOfflineMsgCountLessThan() throws Exception {
-    	// 在线用户给离线用户发送消息
-    	sender.init(null);
-    	Thread.sleep(1000);
-    	for (int i=0; i<9; i++) {
-    		sender.send(Constants.to_account, message+i);
-    	}
-
-    	// 等待消息保存为离线消息
-    	Thread.sleep(1000);
-    	
-    	// 离线用户上线，接收离线消息
-    	receiver.init(new PacketListener() {
-			@Override
-			public void processPacket(String packet) {
-				System.out.println("aaa recv ===> "+counter + "  "+packet);
-				
-				JSONObject jp = JSONObject.fromObject(packet);
-				JSONObject payload = jp.getJSONObject("payload");
-				String content = payload.getString("content");
-				Assert.assertTrue(content.equals(message + counter));
-				
-				// 使用计数器计算离线消息个数
-				counter++;
-			}
-		});
-    	
-    	// 等待离线消息被完全接收
-    	Thread.sleep(2000);
-    	
-    	// 判断离线消息数量
-    	Assert.assertTrue(counter == 9);
-    }
-
-
+    
     @Test
     public void testOfflineMsgCount() throws Exception {
     	// 在线用户给离线用户发送消息
-    	sender.init(null);
+    	sender.init( new PacketListener() {
+			@Override
+			public void processPacket(String packet) {
+				// 发送者不应该收到自己发送的群消息
+				Assert.fail();
+			}
+		});
+
+    	// 在线用户给离线用户发送消息
     	Thread.sleep(1000);
     	for (int i=0; i<20; i++) {
-    		sender.send(Constants.to_account, message+i);
+    		sender.sendToGroup("1", message+i);
     	}
 
     	// 等待消息保存为离线消息
     	Thread.sleep(1000);
     	
-    	// 离线用户上线，接收离线消息
     	receiver.init(new PacketListener() {
 			@Override
 			public void processPacket(String packet) {
@@ -111,6 +82,48 @@ public class OfflineMsgTester {
     	
     	// 判断离线消息数量
     	Assert.assertTrue(counter == Constants.OFFLINE_MSG_COUNT);
+    }
+    
+    @Test
+    public void testOfflineMsgCountLessThan() throws Exception {
+    	// 在线用户给离线用户发送消息
+    	sender.init( new PacketListener() {
+			@Override
+			public void processPacket(String packet) {
+				// 发送者不应该收到自己发送的群消息
+				Assert.fail();
+			}
+		});
+
+    	// 在线用户给离线用户发送消息
+    	Thread.sleep(1000);
+    	for (int i=0; i<9; i++) {
+    		sender.sendToGroup("1", message+i);
+    	}
+
+    	// 等待消息保存为离线消息
+    	Thread.sleep(1000);
+    	
+    	receiver.init(new PacketListener() {
+			@Override
+			public void processPacket(String packet) {
+				System.out.println("aaa recv ===> "+counter + "  "+packet);
+
+				JSONObject jp = JSONObject.fromObject(packet);
+				JSONObject payload = jp.getJSONObject("payload");
+				String content = payload.getString("content");
+				Assert.assertTrue(content.equals(message + counter));
+				
+				// 使用计数器计算离线消息个数
+				counter++;
+			}
+		});
+    	
+    	// 等待离线消息被完全接收
+    	Thread.sleep(2000);
+    	
+    	// 判断离线消息数量
+    	Assert.assertTrue(counter == 9);
     }
 
 
