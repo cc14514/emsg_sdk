bbd046193c16446338da9827f0d1f1c9f73436c6
13611348910 <1047235154@qq.com>
2014-07-25 15:08:14 +0800

    unit test for blacklist

--- emsg-sdk-java/src/test/java/com/emsg/test/blacklist/BlackListTester.java
+++ emsg-sdk-java/src/test/java/com/emsg/test/blacklist/BlackListTester.java
@@ -1,7 +1,5 @@
 package com.emsg.test.blacklist;
 
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import junit.framework.Assert;
@@ -10,15 +8,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.emsg.client.Constants;
+import com.emsg.client.EmsgClient;
+import com.emsg.client.PacketListener;
+import com.emsg.client.beans.DefPacket;
+import com.emsg.client.beans.DefPayload;
+import com.emsg.client.beans.DefProvider;
+import com.emsg.client.beans.IPacket;
 import com.emsg.client.service.BlackList;
 
 /**
- * 使用随机生成账号进行测试（账号A不可能在账号B的黑名单）
- * 1、A不在B的黑名单
- * 2、将A加入到B的黑名单
- * 3、A出现在B的黑名单列表中
- * 4、将A从B的黑名单中删除
- * 5、A不在B的黑名单中
+ * 使用随机生成账号进行测试（账号A和账号B不可能在对方的黑名单）
+ * 1、将A加入到B的黑名单
+ * 2、A和B互相发消息
+ * 3、A收到消息
+ * 4、B收不到消息
  * 
  * @author tiger
  *
@@ -29,63 +33,76 @@ public class BlackListTester {
 	private String userA; 
 	private String userB; 
 	private String pwd = "123123";
+	private EmsgClient<DefPayload> userAClient;
+	private EmsgClient<DefPayload> userBClient;
+	private int counter = 0;
 	
     @Before
     public void setUp() throws Exception {
+    	counter = 0;
+    	
     	// 产生随机账号
     	userA = UUID.randomUUID() + "@test.com";
     	userB = UUID.randomUUID() + "@test.com";
+    	// 将A加入到B的黑名单
+    	bl.append(userA, userB, pwd);
+    	
+    	userAClient = new EmsgClient<>(Constants.server_host,Constants.server_port);
+    	userBClient = new EmsgClient<>(Constants.server_host,Constants.server_port);
+    	
     }
 
     @After
     public void clearup() throws Exception {
-    }
-    
-    @Test
-    public void testBlackList() throws Exception {
-    	// 取B的黑名单
-    	List<Map<String, String>> list = bl.fetch(userB, "123123");
-    	// A不在B的黑名单
-    	Assert.assertFalse(conains(list, userA));
-
-    	// 将A加入到B的黑名单
-    	bl.append(userA, userB, pwd);
     	
-    	Thread.sleep(1000);
-    	// 取B的黑名单
-    	list = bl.fetch(userB, pwd);
-    	// A出现在B的黑名单列表中
-    	Assert.assertTrue(conains(list, userA));
-    	
-    	Thread.sleep(1000);
     	// 将A从B的黑名单中删除
     	bl.remove(userA, userB, pwd);
     	
-    	Thread.sleep(1000);
-    	// 取B的黑名单
-    	list = bl.fetch(userB, pwd);
-    	// A不在B的黑名单
-    	Assert.assertFalse(conains(list, userA));
+    	userAClient.close();
+    	userBClient.close();
     }
     
-    
-    /**
-     * 判断列表中是否包含jid
-     * 
-     * @param list
-     * @param jid
-     * @return
-     */
-    private boolean conains(List<Map<String, String>> list, String jid) {
-    	if (list == null)
-    		return false;
+    @Test
+    public void testFunctionality() throws Exception {
+    	userAClient.setProvider(new DefProvider());
+    	userAClient.setHeartBeat(30000);
+    	userAClient.setPacketListener(new PacketListener<DefPayload>() {
+			@Override
+			public void processPacket(IPacket<DefPayload> packet) {
+				System.out.println("userA packet__recv ===> "+packet);
+				if (packet.getEnvelope().getType() == 1) {
+					// 用户A能收到用户B发的消息
+					counter++;
+				}
+			}
+		});
+    	userAClient.auth(userA, Constants.from_password);
     	
-    	for (Map<String, String> map: list) {
-    		if (jid.equals(map.get("jid"))) {
-    			return true;
-    		}
-    	}
-    	return false;
+    	
+    	userBClient.setProvider(new DefProvider());
+    	userBClient.setHeartBeat(30000);
+    	userBClient.setPacketListener(new PacketListener<DefPayload>() {
+			@Override
+			public void processPacket(IPacket<DefPayload> packet) {
+				System.out.println("userB packet__recv ===> "+packet);
+				if (packet.getEnvelope().getType() == 1) {
+					// 用户B不能收到用户A发的消息
+					Assert.fail();
+				}
+			}
+		});
+    	userBClient.auth(userB, Constants.from_password);
+    	
+    	// 用户A给用户B发消息
+    	userAClient.send(new DefPacket(userB, "test", 1));
+    	// 用户B给用户A发消息
+    	userBClient.send(new DefPacket(userA, "test", 1));
+
+    	
+    	Thread.sleep(1000);
+    	Assert.assertEquals(counter, 1);
     }
+    
+    
     
 }
